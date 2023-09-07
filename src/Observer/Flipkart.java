8579e52f393cf95a21885c327b657b20c5b32580
Keshav Walke <kkeshavwalke@gmail.com>
2023-09-07 21:15:04 +0530

    next commit

--- src/Observer/Flipkart.java
+++ src/Observer/Flipkart.java
@@ -28,5 +28,6 @@ public class Flipkart {
         for(OnOrderPlacedSubscriber onOrderPlacedSubscriber : onOrderPlacedSubscribers){
             onOrderPlacedSubscriber.announceOrderPlaced();
         }
+
     }
 }
